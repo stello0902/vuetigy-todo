8f9e9ba522682e35f4d3266cded72673d922eed0
stello mthembu <stello0902@gmail.com>
2023-04-23 10:32:44 +0200

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,174 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+      </q-toolbar>
+
+      <div class="q-px-lg q-pt-xl q-mb-md">
+        <div class="text-h3">Tudo</div>
+        <div class="text-subtitle1">{{ todaysDate }}</div>
+
+      </div>
+      <q-img
+      src="statics/hintersee.jpg"
+      class="header-image absolute-top"
+      />
+
+
+    </q-header>
+
+    <q-drawer
+        v-model="leftdrawerOpen"
+        show-if-above
+        :width="250"
+        :breakpoint="600"
+      >
+        <q-scroll-area style="height: calc(100% - 192px); margin-top: 192px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item
+              to="/"
+              exact
+              clickable v-ripple>
+
+              <q-item-section avatar>
+                <q-icon name="list" />
+              </q-item-section>
+
+              <q-item-section>
+                Todo
+              </q-item-section>
+            </q-item>
+
+
+            <q-item
+            to="/help"
+            exact
+            clickable v-ripple>
+
+               <q-item-section avatar>
+                <q-icon name="help"/>
+              </q-item-section>
+
+              <q-item-section>
+                Help
+              </q-item-section>
+            </q-item>
+          </q-list>
+
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="statics/hintersee.jpg" style="height: 192px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="https://www.gravatar.com/userimage/234433005/4b0ef8254a85428a60d4ca4b47d7ff31?size=120">
+            </q-avatar>
+            <div class="text-weight-bold">Stello Mthembu</div>
+            <div>@Stello02</div>
+          </div>
+        </q-img>
+
+    </q-drawer>
+
+    <q-page-container>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { date } from 'quasar'
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  },
+    computed: {
+      todaysDate() {
+        let timeStamp = Date.now()
+        return date.formatDate(timeStamp, 'dddd D MMMM')
+      }
+    }
+
+
+})
+</script>
+<style lang="scss">
+  .header-image {
+    height: 100%;
+    z-index: -1;
+    opacity: 0.2;
+    filter: grayscale(100%)
+  }
+
+</style>
---
+++ src/pages/Help.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-pa-lg">
+    <h5 class="qmt-mt-none">Help</h5>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet accusantium beatae facere excepturi? Culpa dolorem ratione vero dolores ipsam, quibusdam eveniet hic id perferendis minima laudantium repellendus voluptatum distinctio veniam.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur cupiditate autem perspiciatis expedita dolorum. Nam commodi sequi ducimus rem earum vel cumque quod officiis ratione. Officiis molestias delectus ducimus quod.</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+
+})
+</script>
